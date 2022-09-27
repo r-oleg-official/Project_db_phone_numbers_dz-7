31ca1164d047c80be913b866c17ec692d412300c
Alexander Kotegov <s.kotegov2010@yandex.ru>
2022-09-27 16:55:17 +0500

    added modules

---
+++ gui.py
@@ -0,0 +1,11 @@
+def view_data(data):
+    print(data)
+
+def get_fname():
+    return input('first name = ')
+
+def get_lname():
+    return input('last name = ')
+
+def get_phone_num():
+    return input('phone number = ')
